1ffe46e0962648574ca35ebf5d9761459da50eb9
Anas Al-Mughamsi <Anas.Mughamsi@gmail.com>
2023-10-25 22:38:37 +0300

    first push

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import TestimonialsLayout from './components/TestimonialsLayout/TestimonialsLayout'
+
+function App() {
+
+  return (
+    <>
+    <TestimonialsLayout/>
+    </>
+  )
+}
+
+export default App
